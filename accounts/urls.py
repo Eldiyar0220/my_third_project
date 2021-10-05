0920aa9d759c20fd016969f365f785ff39e7591a
Your Name <Ebolotov904@gmail.com>
2021-10-06 02:17:47 +0600

    registration on CBV

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from accounts import views
+from accounts.views import RegisterViews
 
 urlpatterns = [
-   path('register/', views.register, name='register'),
-   # path('register/', RegisterViews.as_view(), name='register'),
+   path('register/', RegisterViews.as_view(), name='register'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from accounts.forms import UserRegistrationForm
 
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            new_form = form.save(commit=False)
-            new_form.set_password(form.cleaned_data['password'])
-            new_form.save()
-            return HttpResponse('Вы успешно заргистрировались')
-        return render(request, 'pages/registration.html', {'form':form})
-    else:
-        form = UserRegistrationForm()
-        return render(request, 'pages/registration.html', {'form':form})
+class RegisterViews(CreateView):
+    template_name = 'pages/registration.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('home')
